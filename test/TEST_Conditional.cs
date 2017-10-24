7adb8f4ae1bf97c929005e44320b12f85653fd05
Scott Sherman <scott@loki3.com>
2017-10-23 20:11:57 -0700

    Revert "have l3.ifBody indicate whether it evaluated the body it was given"
    This reverts commit b0acbc83b6d72db4451faca1e94c0b1e0e7826c5.

--- test/TEST_Conditional.cs
+++ test/TEST_Conditional.cs
@@ -28,16 +28,12 @@ namespace loki3.builtin.test
 
 			{	// run body
 				Value value = TestSupport.ToValue("l3.ifBody l3.createMap [ :do? true :body [ ' 5 ' ] ]", scope);
-                var array = value.AsArray;
-				Assert.AreEqual(5, array[0].AsInt);
-				Assert.AreEqual(true, array[1].AsBool);
+				Assert.AreEqual(5, value.AsInt);
 			}
 			{	// skip body
 				Value value = TestSupport.ToValue("l3.ifBody l3.createMap [ :do? false :body [ ' 5 ' ] ]", scope);
-                var array = value.AsArray;
-                Assert.IsTrue(array[0].IsNil);
-                Assert.AreEqual(false, array[1].AsBool);
-            }
-        }
+				Assert.AreEqual(false, value.AsBool);
+			}
+		}
 	}
 }
